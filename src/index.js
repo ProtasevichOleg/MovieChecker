f8220f6b61da9bc5af2e67a8ca036c6541a1f1b7
Oleh Protasevych <protasevicholeg@gmail.com>
2023-06-07 02:45:16 +0300

    Try to fix deploy

--- src/index.js
+++ src/index.js
@@ -18,11 +18,11 @@ const theme = {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter basename="/goit-react-hw-05-movies">
         <App />
-      </ThemeProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
